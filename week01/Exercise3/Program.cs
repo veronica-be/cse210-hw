0da782676bf763e2c921cc1558b32754cd0981f8
veronica-be <verobees@icloud.com>
2026-05-08 09:40:19 -0500

    Excersise 3 (game to guess ramdon number): topics used: do-while loop, the use of random class, variables, nested loops, if statements

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -3,7 +3,44 @@ using System;
 class Program
 {
     static void Main(string[] args)
+
+
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        int guess;
+        int totalGuesses = 0;
+        string response;
+
+        do
+        {
+            Random randomNumber = new Random();
+            int magicNumber = randomNumber.Next(1, 100);
+            Console.WriteLine();
+
+            do
+            {
+                Console.Write("What is your guess? ");
+                guess = int.Parse(Console.ReadLine());
+                totalGuesses += 1;
+
+                if (guess > magicNumber)
+                {
+                    Console.WriteLine("Lower");
+                }
+                else if (guess < magicNumber)
+                {
+                    Console.WriteLine("Higher");
+                }
+                else
+                {
+                    Console.WriteLine("You've got it!!!");
+                    Console.WriteLine($"Total of guesses: {totalGuesses}");
+                }
+            } while (guess != magicNumber);
+
+            Console.WriteLine();
+            Console.Write("Do you want to play again? ");
+            response = Console.ReadLine();
+            totalGuesses = 0;
+        } while (response == "yes");
     }
 }
