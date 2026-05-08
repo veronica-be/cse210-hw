7f791c5c6bc262510496cb5c6ffece25a0ad148b
veronica-be <verobees@icloud.com>
2026-05-08 18:11:38 -0500

    Exercise 5: Function practice. Topics: making functions (declare return value, static keyword, parameters). Math.Pow(x, ^) learned by web. (int) too

--- week01/Exercise5/Program.cs
+++ week01/Exercise5/Program.cs
@@ -4,6 +4,47 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise5 Project.");
+        static void DisplayWelcome()
+        {
+            Console.WriteLine("Welcome to the Program!");
+        }
+
+        static string PromptUserName()
+        {
+            Console.Write("Please enter your name: ");
+            string name = Console.ReadLine();
+            return name;
+        }
+
+        static int PromptUserNumber()
+        {
+            Console.Write("Please enter your favorite number: ");
+            int number = int.Parse(Console.ReadLine());
+            return number;
+        }
+
+        static int SquareNumber(int numberGiven)
+        {
+            int result = (int)Math.Pow(numberGiven, 2); //I know I could just multiply it, but I like to learn new ways to do it.
+            return result;
+        }
+
+        static void DisplayResult(string userName, int squaredNumber)
+        {
+            Console.WriteLine($"{userName}, the square of your number is {squaredNumber}");
+        }
+
+        static void Main()
+        {
+            DisplayWelcome();
+
+            string name = PromptUserName();
+            int number = PromptUserNumber();
+            int squaredNumber = SquareNumber(number);
+
+            DisplayResult(name, squaredNumber);
+        }
+
+        Main();
     }
 }
