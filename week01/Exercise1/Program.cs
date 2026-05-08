c607fc663b0abe47679160c639af522a6edbe83b
veronica-be <verobees@icloud.com>
2026-05-07 23:17:00 -0500

    Exercise 1 done, console.write /writeline and ReadLine used

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -4,6 +4,13 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise1 Project.");
+        Console.Write("What is your first name? ");
+        string name = Console.ReadLine();
+
+        Console.Write("What is your last name? ");
+        string LastName = Console.ReadLine();
+
+        Console.WriteLine();
+        Console.Write($"Your name is {LastName}, {name} {LastName}.");
     }
 }
