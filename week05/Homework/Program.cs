fcac21e0306a4c2821c8d2cc68c569559677ec43
veronica-be <verobees@icloud.com>
2026-06-02 10:02:29 -0500

    Homework Program W05 (Classes: Math , Writing and Assignment)

--- week05/Homework/Program.cs
+++ week05/Homework/Program.cs
@@ -4,6 +4,17 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Homework Project.");
+        Console.WriteLine("Hello World! This is the Homework Project." + Environment.NewLine);
+
+        MathAssignment mathAssignment = new MathAssignment("Veronica Becerra", "Fractions", "7.3", "8 — 19");
+        Console.WriteLine(mathAssignment.GetSummary());
+        Console.WriteLine(mathAssignment.GetHomeworkList());
+
+        Console.WriteLine();
+
+        WritingAssignment writingAssignment = new WritingAssignment("Veronica Becerra", "European History", "The Causes of World War II");
+        Console.WriteLine(writingAssignment.GetSummary());
+        Console.WriteLine(writingAssignment.GetWritingInformation());
+
     }
 }
