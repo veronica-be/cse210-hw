a64e2e24ca487b361300a606ee790e3b8e5e07ba
veronica-be <verobees@icloud.com>
2026-06-09 23:13:48 -0500

    Polymorphism Activity

--- week06/Shapes/Program.cs
+++ week06/Shapes/Program.cs
@@ -5,5 +5,23 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Shapes Project.");
+
+        List<Shape> shapes = new List<Shape>();
+
+        Square square = new Square("red", 55.3);
+        Rectangle rectangle = new Rectangle("blue", 12, 2);
+        Circle circle = new Circle("gold", 5);
+
+        shapes.Add(square);
+        shapes.Add(rectangle);
+        shapes.Add(circle);
+
+        foreach (var shape in shapes)
+        {
+            Console.Write("Color: " + shape.GetColor() + "     ");
+            Console.WriteLine("Area:" + shape.GetArea());
+
+        }
+
     }
 }
---
+++ week06/Shapes/Rectangle.cs
@@ -0,0 +1,16 @@
+public class Rectangle : Shape
+{
+    private double _lenght;
+    private double _width;
+
+    public Rectangle(string color, double lenght, double width) : base(color)
+    {
+        _lenght = lenght;
+        _width = width;
+    }
+
+    public override double GetArea()
+    {
+        return _lenght * _width;
+    }
+}
